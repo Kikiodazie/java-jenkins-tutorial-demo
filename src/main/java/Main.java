4a5c6f2726cf19424106943fe7576690330c653d
Kikiodazie <kiki.odazie@yahoo.com>
2021-05-12 00:30:23 +0100

    added readme,main class, and test class

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(checkIfInputIsAnEvenNumber(122));
+    }
+
+    public static boolean checkIfInputIsAnEvenNumber(int number){
+        return number % 2 == 0;
+    }
+}
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestMain {
+
+
+    @Test
+    public void testInputIsEven(){
+        assertTrue(Main.checkIfInputIsAnEvenNumber(22));
+    }
+}
